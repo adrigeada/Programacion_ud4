2ed7000dbf839918caaad9cf2f534837ddd8a4b5
A709\dam1 <adrigeada@gmail.com>
2025-12-04 18:15:46 +0100

    Bateria funciones

--- programacion_ud4/src/main/java/org/example/Ejercicio4.java
+++ programacion_ud4/src/main/java/org/example/Ejercicio4.java
@@ -15,10 +15,15 @@ public class Ejercicio4 {
 
             for (int j = 0; j < dia.length; j++) {
 
-                System.out.println(dia[j]+ " ");
+                System.out.println(dia[j]+ "€ ");
             }
 
-            masVentas(dia);
+        System.out.println("El día con mas ventas es: "+ masVentas(dia));
+        System.out.println("El día con menos ventas es: " +menosVentas(dia));
+        System.out.println("La media de toda la semana es: "+ mediaSemanal(dia)+ "€");
+        System.out.println("La recaudacion del domingo es: "+recaudacionDomingo(dia));
+
+
 
 
 
@@ -27,38 +32,110 @@ public class Ejercicio4 {
     public static double[] rellenarVector(){
 
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Escribe los valores de la semana");
-        String[] semana = teclado.nextLine().split(" ");
 
-        if (semana.length == 6){
+        double[] dia = new double[6];
+        do {
+            System.out.println("Escribe los valores de la semana");
+            String[] semana = teclado.nextLine().split(" ");
 
-        }
-        double[] dia = new double[semana.length];
+            dia = new double[semana.length];
+            if (semana.length == 6){
 
-        for (int i = 0; i < semana.length; i++) {
+                for (int i = 0; i < semana.length; i++) {
 
-             dia[i] =  Double.parseDouble(semana[i])  ;
-        }
+                    dia[i] =  Double.parseDouble(semana[i])  ;
+                }
+
+                break;
+
+            }else {
+                System.out.println("Introduce 6 dias");
+            }
+        }while (true);
+
 
      return dia;
     }
 
-    public static void masVentas (double[] dia){
+    public static String masVentas (double[] dia){
 
         double[] copia = dia.clone();
+        String[] dias_semana = {"MARTES","MIERCOLES","JUEVES","VIERNES","SABADO","DOMINGO"};
         double aux = 0;
+        String aux1 = "";
 
         for (int i = 0; i < copia.length; i++) {
 
             if (copia[i] > aux){
 
                 aux = copia[i];
+                aux1 = dias_semana[i];
             }
 
         }
-        System.out.println(aux + " Es el valor de mas ventas");
+//        System.out.println(aux + " Es el valor de mas ventas"+"en el día "+aux1);
 
 
+        return aux1;
+    }
+
+    public static String menosVentas (double[] dia){
+
+        double aux_menor = 50000;
+        int aux_posicion = 0;
+        for (int i = 0; i < dia.length; i++) {
+
+            if (dia[i] < aux_menor){
+
+                aux_menor = dia[i];
+                aux_posicion = i;
+            }
+        }
+
+        switch (aux_posicion){
+
+            case 0:
+                return "MARTES";
+            case 1:
+                return "MIERCOLES";
+            case 2:
+                return "JUEVES";
+            case 3:
+                return "VIERNES";
+            case 4:
+                return "SABADO";
+            case 5:
+                return "DOMINGO";
+
+        }
+
+
+        return "a";
+    }
+
+    public static double mediaSemanal(double[] dia){
+
+        double[] media = dia.clone();
+        double resultadoMedia = 0;
+
+        for (int i = 0; i < media.length; i++) {
+
+            resultadoMedia += media[i];
+
+        }
+        resultadoMedia = resultadoMedia/media.length;
+
+        return resultadoMedia;
+    }
+
+    public static double recaudacionDomingo(double[] dia){
+
+        double media = 0;
+
+        media = dia[dia.length-1];
+
+
+        return media;
 
     }
 
