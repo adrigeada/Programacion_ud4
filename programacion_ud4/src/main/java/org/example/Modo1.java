55780f639a1be1ecd836aaf6221c952b74a608ce
DESKTOP-MVTPKEL\Adrian Geada <adrigeada@gmail.com>
2025-12-18 10:54:40 +0100

    Modo 1

--- programacion_ud4/src/main/java/org/example/Modo1.java
+++ programacion_ud4/src/main/java/org/example/Modo1.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Modo1 {
+
+    static Scanner teclado;
+
+    public static void main(String[] args) {
+
+        teclado = new Scanner(System.in);
+
+        int numCasos = teclado.nextInt();
+        for (int i = 0; i < numCasos; i++){
+            casoDePrueba();
+        }
+            
+    }
+
+    public static void casoDePrueba() {
+
+        // TU CÓDIGO AQUÍ
+
+    } // casoDePrueba
 }
