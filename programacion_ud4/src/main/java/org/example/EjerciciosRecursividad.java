d23ce93ddf8e93f8c6460fb6db33102f1b7c1082
A709\dam1 <adrigeada@gmail.com>
2026-01-08 19:38:08 +0100

    Ejrecursividad

---
+++ programacion_ud4/src/main/java/org/example/EjerciciosRecursividad.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class EjerciciosRecursividad {
+    static Scanner teclado = new Scanner(System.in);
+
+    static void main() {
+
+        imprimir_menu();
+        elegir_opcion();
+        repetir();
+
+    }
+
+    static void imprimir_menu(){
+
+        System.out.println("*** Batería de ejercicios sobre recursividad ***");
+        System.out.println("Selecciona a continuación el modo que quieras ejecutar...");
+        System.out.println("1 - Dígitos");
+        System.out.println("2 - Potencias");
+        System.out.println("3 - Del Revés");
+        System.out.println("4 - Binario");
+        System.out.println("5 - A binario");
+        System.out.println("6 - Orden alfabético");
+        System.out.println("7 - Mostrar suma");
+
+    }
+
+    static void elegir_opcion(){
+
+        char modo = teclado.next().charAt(0);
+
+        switch (modo){
+
+            case '1':
+                System.out.println(ej1(4561));
+                break;
+            case '2':
+                System.out.println(ej2(3,4));
+                break;
+            case '3':
+
+                break;
+            case '4':
+
+                break;
+            case '5':
+
+                break;
+            case '6':
+
+                break;
+            case '7':
+
+                break;
+
+            default:
+        }
+
+    }
+
+    static void repetir (){
+        System.out.println("Elige una opción:");
+        System.out.println("[M] - Volver al menú principal");
+        System.out.println("[X] - Salir");
+
+        char opcion = teclado.next().charAt(0);
+
+        if (opcion == 'M') {
+            main();
+        }
+    }
+
+    static int ej1 (int n){
+
+        if (n < 10){
+            return 1 ;
+        }else {
+             return 1+ej1(n/10);
+
+        }
+
+
+    }
+
+    static int ej2 (int base,int exponente){
+
+        if (exponente == 1){
+            return base;
+        }else{
+
+           return base*ej2(base,exponente-1);
+        }
+
+    }
+
+    static int ej3 (int num){
+
+        if (num < 10){
+            return num;
+        }else {
+            num = num%10;
+            return ej3(num/10);
+        }
+        
+    }
+
+}
--- programacion_ud4/src/main/java/org/example/ej2Recursividad.java
+++ programacion_ud4/src/main/java/org/example/ej2Recursividad.java
@@ -6,10 +6,43 @@ public class ej2Recursividad {
     static int limite = 10;
 
     static void main() {
+
+        System.gc(); //Limpia lo que pueda antes de medir
+
+        long inicioEjecucion = System.nanoTime();
+        long memoriaAntes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+
         contar(1);
+
+        long finEjecucion = System.nanoTime();
+        long memoriaDespues = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        System.out.println("Tiempo recursivo: " + (finEjecucion - inicioEjecucion) + " ns");
+        System.out.println("Memoria consumida recursivo: " + (memoriaDespues - memoriaAntes) + " bytes");
+
+        inicioEjecucion = System.nanoTime();
+        memoriaAntes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        contar_bucle();
+
+        finEjecucion = System.nanoTime();
+        memoriaDespues = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        System.out.println("Tiempo bucle: " + (finEjecucion - inicioEjecucion) + " ns");
+        System.out.println("Memoria consumida bucle: " + (memoriaDespues - memoriaAntes) + " bytes");
+
+//        System.out.println(sumar(1));
 
     }
 
+    public static void contar_bucle(){
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
 
     static void contar (int inicio){
 
@@ -19,4 +52,14 @@ public class ej2Recursividad {
         }
 
     }
+
+    static int sumar (int num) {
+
+        if (num < limite){
+            num += sumar(num+1);
+        }
+
+        return num;
+    }
+
 }
